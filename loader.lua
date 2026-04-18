4362895f73eb30d59e4c0531b3eed1bc92fcab49
muie12344 <bogdanandrei5687@gmail.com>
2026-04-18 15:10:47 +0300

    Create loader.lua for BloxStrike Domination
    Added a loader script for BloxStrike Domination to load from GitHub.

---
+++ loader.lua
@@ -0,0 +1,9 @@
+--[[
+    BloxStrike Domination - Loader
+    Paste this into your executor to load the script from GitHub.
+    
+    Replace YOUR_GITHUB_USERNAME with your actual GitHub username
+    and REPO_NAME with your repository name.
+]]
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/YOUR_GITHUB_USERNAME/REPO_NAME/main/bloxstrike_hub.lua"))()
